a5b3c42ebd3a913a72a6660e83cecc6af25f4e47
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2022-11-20 15:58:19 +0700

    login with context

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,19 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { useState } from "react";
 
 import Navbar from "../components/navbar";
+import { AuthContext } from "../utils/AuthContext";
 // import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const [token, setToken] = useState(null);
+
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <AuthContext.Provider value={{token, setToken}}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </AuthContext.Provider>
   );
 }
 
